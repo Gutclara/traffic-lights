3f97fc51d2d2e39e105ff75aa146f0e5128fa8ad
Gutclara <mariadelasclaras@gmail.com>
2021-04-29 19:11:02 +0000

    alguito mas

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+/*import rigoImage from "../../img/rigo-baby.jpg";*/
 
 //create your first component
 export function Home() {
+	/*const [variable,funcion(que modifica mi varible)] = useState(aca pongo el estado inicial o por defecto, si quiero, sino no pongo nada)*/
+	const [color, setColor] = useState();
 	return (
 		<div className="container bg-dark">
 			<div className="color-red">
-				<button type="button" className="btn btn-danger"></button>
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={() => setColor("red")}></button>
 			</div>
 			<div className="color-yellow">
-				<button type="button" className="btn btn-warning"></button>
+				<button
+					type="button"
+					className="btn btn-warning"
+					onClick={() => setColor("yellow")}></button>
 			</div>
 			<div className="color-green">
-				<button type="button" className="btn btn-info"></button>
+				<button
+					type="button"
+					className="btn btn-info"
+					onClick={() => setColor("green")}></button>
 			</div>
 		</div>
 	);
