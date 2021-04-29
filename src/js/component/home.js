c27b9bfcd9e225d3634c431611c9aa1ca7c53cd7
Gutclara <mariadelasclaras@gmail.com>
2021-04-29 12:31:19 +0000

    arranco

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark">
+			<div className="color-red">
+				<button type="button" className="btn btn-danger"></button>
+			</div>
+			<div className="color-yellow">
+				<button type="button" className="btn btn-warning"></button>
+			</div>
+			<div className="color-green">
+				<button type="button" className="btn btn-info"></button>
+			</div>
 		</div>
 	);
 }
